bb0b9254df1974128cf2e99f361ab7468a4aa725
Ben Gitles <bengitles@gmail.com>
2012-11-18 15:26:17 -0500

    Tried getting SendGrid to work

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,14 +1,15 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  # default from: "from@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifications.new_app.subject
   #
+  
   default from: "bengitles@gmail.com"
   def new_app
     @greeting = "Hi"
-    mail to:"gitlesb@seas.upenn.edu"
+    mail to: "bengitles@gmail.com"
   end
 end
